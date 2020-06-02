2b2c7208d5a14bbea8c9055b76fec9c5c5d51432
TKS0522 <t_o_19880522@outlook.com>
2020-06-02 13:12:53 +0900

    init

---
+++ WpfAppDeployUsingAzureDevOps/WpfAppDeployUsingAzureDevOps/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppDeployUsingAzureDevOps
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            FileVersionInfo versionInfo = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location);
+            string version = versionInfo.FileVersion;
+            label.Content = version;
+        }
+    }
+}
