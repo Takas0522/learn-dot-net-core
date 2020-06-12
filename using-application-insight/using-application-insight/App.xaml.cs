932d9de11e87804c89677dcd3c3c75e4addca236
TKS0522 <t_o_19880522@outlook.com>
2020-06-12 12:09:56 +0900

    add

---
+++ using-application-insight/using-application-insight/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace using_application_insight
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            TelemetryConfiguration configuration = TelemetryConfiguration.CreateDefault();
+            configuration.InstrumentationKey = "<InstrumentationKey>";
+            var telemetryClient = new TelemetryClient(configuration);
+            telemetryClient.TrackTrace("Application Run");
+            telemetryClient.TrackEvent("My Event");
+            try
+            {
+                throw new Exception("CUSTOM ERROR");
+            }
+            catch (Exception e)
+            {
+                telemetryClient.TrackException(e);
+            }
+            
+        }
+    }
+}
